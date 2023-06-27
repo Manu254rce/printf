2a76936f24574c222c56f9b5b27e6eefe2f08001
Manu254rce <straussmoabit@gmail.com>
2023-06-27 05:23:04 -0700

    C printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,37 @@
 # include "main.h"
 
 /**
+ * format_str - funcion ha handles he ieraion for specifing formas
+ * @format: he forma specified
+ * @chars: variadic lis pe
+ * @type_list: sruc for he formas
+ * @count: poiner ha keeps rack of characers
+ */
+
+void format_str(const char *format, va_list chars, _fmt *type_list, int *count)
+{
+	int i, j;
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			++i;
+			for (j = 0; type_list[j].specifier != NULL; j++)
+			{
+				if (*type_list[j].specifier == format[i])
+				{
+					type_list[j].handler(chars, count);
+					break;
+				}
+			}
+		}
+		else
+			putchar(format[i]);
+	}
+}
+
+/**
  * _printf - function that printd output given a format specifier
  * @format: character argument
  * Return: 'count' the number of strings printed
@@ -9,13 +40,14 @@
 int _printf(const char *format, ...)
 {
 	int count = 0;
-	int i, j;
 	va_list chars;
+
 	_fmt type_list[] = {
 		{"c", handle_char},
 		{"s", handle_str},
 		{"d", handle_int},
 		{"i", handle_int},
+		{"u", handle_unsigned},
 		{"b", handle_bin},
 		{"o", handle_octal},
 		{"x", handle_hex_lower},
@@ -28,24 +60,10 @@ int _printf(const char *format, ...)
 	};
 
 	va_start(chars, format);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			++i;
-			for (j = 0; type_list[j].specifier != NULL; j++)
-			{
-				if (*type_list[j].specifier == format[i])
-				{
-					type_list[j].handler(chars, &count);
-					break;
-				}
-			}
-		}
-		else
-			putchar(format[i]);
+	format_str(format, chars, type_list, &count);
 
-	}
 	va_end(chars);
+
 	return (count);
 }
+
